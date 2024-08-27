cef17c12021e126c4fab8e39b5592247c3906937
banelengwane <banelengwane@gmail.com>
2024-08-27 23:39:17 +0200

    Applying box model: spacing, boarder and display

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,8 +4,8 @@ import Username from "../features/user/Username"
 
 function Header() {
     return (
-        <header className="bg-yellow-500 uppercase">
-            <Link to='/' className="tracking-widest">Fast React Pizza Co.</Link>
+        <header className="bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200">
+            <Link to='/' className="tracking-widest font-semibold">Fast React Pizza Co.</Link>
             <SearchOrder />
             <Username />
         </header>
